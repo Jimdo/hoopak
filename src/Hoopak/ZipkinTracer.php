7ab49d96f33a888a634e661282d17c50574841c2
Jan Brauer <jan@jimdo.com>
2012-08-26 21:54:49 +0200

    Implement record

--- src/Hoopak/ZipkinTracer.php
+++ src/Hoopak/ZipkinTracer.php
@@ -5,15 +5,30 @@
  **/
 class ZipkinTracer
 {
+    // TODO properly import thrift constants
+    private _endAnnotations = array("cr", "ss");
     
     public function __construct($scribeClient, $category="zipkin")
     {
         $this->_scribe = $scribeClient;
         $this->_category = $category;
+        $this->_annotationsForTrace = array();
     }
 
-    public function sendTrace($trace, $annotation)
+    public function sendTrace($trace, $annotations)
     {
 
+    }
+
+    public function record($trace, $annotation)
+    {
+        $traceKey = array($trace->traceId, $trace->spanId);
+        $this->_annotationsForTrace[$traceKey][] = $annotation;
+
+        if in_array($annotation->name, $this->_endAnnotations) {
+            $annotations = $this->_annotationsForTrace[$traceKey];
+            unset($this->_annotationsForTrace[$traceKey]);
+            $this->sendTrace($trace, $annotations);
+        }
     }
 }
