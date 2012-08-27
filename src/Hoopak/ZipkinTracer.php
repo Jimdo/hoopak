79b69fc547be420db35f27344d91dd88fd9fe5ae
Jan Brauer <jan@jimdo.com>
2012-08-27 10:33:58 +0200

    Base64 encode the Thrift for Scribe

--- src/Hoopak/ZipkinTracer.php
+++ src/Hoopak/ZipkinTracer.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Hoopak;
+
 /**
  * 
  **/
@@ -17,7 +19,8 @@ class ZipkinTracer
 
     public function sendTrace($trace, $annotations)
     {
-
+        $thriftOut = $this->thriftToBase64($trace, $annotations);
+        $this->_scribe->log($this->category, $thriftOut);
     }
 
     public function record($trace, $annotation)
@@ -30,5 +33,54 @@ class ZipkinTracer
             unset($this->_annotationsForTrace[$traceKey]);
             $this->sendTrace($trace, $annotations);
         }
+    }
+
+    private function thriftToBase64($trace, $annotations)
+    {
+        $thriftAnnotations = array();
+        $binaryAnnotations = array();
+
+        foreach ($annotations as $annotation) {
+            $host = null;
+            if ($annotation->endpoint) {
+                host = new Endpoint(
+                    array(
+                        "ipv4" => annotation->ipv4,
+                        "port" => annotation->port,
+                        "service_name" => annotation->port,
+                    )
+                );
+            }
+
+            if ($annotation->annotationType == 'timestamp') {
+                $thriftAnnotations[] = new Annotation(
+                    array(
+                        "timestamp" => $annotation->value,
+                        "value" => $annotation->name,
+                        "host" => $host
+                    )
+                );
+            } else {
+                //TODO handle binary annotations
+            }
+        }
+
+        $thriftTrace = new Span(
+            array(
+                "trace_id" => $trace->traceId,
+                "name" => $trace->name,
+                "id" => $trace->spanId,
+                "parent_id" => $trace->parentId,
+                "annotations" => $thriftAnnotations,
+                "binary_annotations" => $binaryAnnotations
+            )
+        )
+
+        $trans = new TMemoryBuffer();
+        $proto = new TBinaryProtocol(trans);
+
+        $thriftTrace->write($proto);
+
+        return base64_encode($trans->getBuffer())
     }
 }
