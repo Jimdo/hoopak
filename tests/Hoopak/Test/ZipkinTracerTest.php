0f81d49b249bcd334da7f9981e6d832a93c0bb4e
Jan Brauer <jan@jimdo.com>
2012-08-28 23:10:13 +0200

    Abuse test for testing

--- tests/Hoopak/Test/ZipkinTracerTest.php
+++ tests/Hoopak/Test/ZipkinTracerTest.php
@@ -5,6 +5,7 @@ namespace Hoopak\Test;
 use Hoopak\Annotation;
 use Hoopak\Endpoint;
 use Hoopak\Trace;
+use Hoopak\ScribeClient;
 use Hoopak\ZipkinTracer;
 
 class ZipkinTracerTest extends \PHPUnit_Framework_TestCase
@@ -14,22 +15,32 @@ class ZipkinTracerTest extends \PHPUnit_Framework_TestCase
      */
     public function aTraceIsBase64EncodedWhenSendingItToScribe()
     {
-        $scribe = new ScribeMock();
+        $scribe = new ScribeClient("werner", 9410);
         $zipkinTracer = new ZipkinTracer($scribe);
-        $trace = new Trace("foo", null, null, null, array($zipkinTracer));
-        $trace->setEndpoint(new Endpoint("1.2.3.4", "8000", "service"));
+        $trace = new Trace("geloet", null, null, null, array($zipkinTracer));
+        $trace->setEndpoint(new Endpoint("1.2.3.4", "8000", "a-client"));
+        $trace->record(Annotation::string("spam", "eggs"));
+        $trace->record(Annotation::clientSend());
+        sleep(3);
+        $trace1 = $trace->child("heavy-lifting");
+        $trace1->setEndpoint(new Endpoint("10.1.2.3", "80", "the-service"));
+        $trace1->record(Annotation::serverReceive());
+        sleep(1);
+        $trace1->record(Annotation::serverSend());
+        sleep(5);
         $trace->record(Annotation::clientReceive());
-        $this->assertEquals("foo", $scribe->message);
+        //print_r($scribe->messages);
+        //$this->assertEquals("foo", $scribe->messages);
     }
 }
 
 class ScribeMock
 {
-    public $message;
+    public $messages = array();
 
     public function log($category, $message)
     {
-        $this->message = $message;
+        $this->messages[] = $message;
     }
 
 }
