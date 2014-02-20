203b9531ef5b11b5dfb4f248178f337f464466bc
Jan Brauer <jan@jimdo.com>
2014-02-20 21:57:19 +0100

    Use usleep, add missing generated sources

--- tests/Hoopak/Test/ZipkinTracerTest.php
+++ tests/Hoopak/Test/ZipkinTracerTest.php
@@ -15,19 +15,19 @@ class ZipkinTracerTest extends \PHPUnit_Framework_TestCase
      */
     public function aTraceIsBase64EncodedWhenSendingItToScribe()
     {
-        $scribe = new ScribeClient("werner", 9410);
+        $scribe = new ScribeClient("localhost", 9410);
         $zipkinTracer = new ZipkinTracer($scribe);
         $trace = new Trace("geloet", null, null, null, array($zipkinTracer));
         $trace->setEndpoint(new Endpoint("1.2.3.4", "8000", "a-client"));
         $trace->record(Annotation::string("spam", "eggs"));
         $trace->record(Annotation::clientSend());
-        sleep(3);
+        usleep(3000);
         $trace1 = $trace->child("heavy-lifting");
         $trace1->setEndpoint(new Endpoint("10.1.2.3", "80", "the-service"));
         $trace1->record(Annotation::serverReceive());
-        sleep(1);
+        usleep(1000);
         $trace1->record(Annotation::serverSend());
-        sleep(5);
+        usleep(500);
         $trace->record(Annotation::clientReceive());
         //print_r($scribe->messages);
         //$this->assertEquals("foo", $scribe->messages);
